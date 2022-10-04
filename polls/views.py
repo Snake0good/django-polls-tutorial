90dfcb1287bb6ee70a81a920f501917b1561566e
Jacob Good <jacobegood@gmail.com>
2022-10-03 22:07:13 -0400

    results views up and running

--- polls/views.py
+++ polls/views.py
@@ -12,32 +12,18 @@ from .models import Question, Choice
 # Create your views here.
 def index(request): 
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    
-    # template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    
-    # return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-
-    # VERY COMMON ABOVE -> reWritten below
     question = get_object_or_404(Question, pk=question_id)
-
     return render(request, 'polls/detail.html', { 'question':question })
-    # return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "you're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', { 'question':question })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -51,5 +37,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponse(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
